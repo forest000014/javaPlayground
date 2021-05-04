19b843de0e0e9bb2bed11cc6a5ef83892e085554
macbook <macbook@localhost>
2021-05-04 17:11:54 +0900

    changed 메시지 추가

--- src/HelloWorld2.java
+++ src/HelloWorld2.java
@@ -2,6 +2,7 @@
 public class HelloWorld2 {
 	public static void main(String[] args) {
 		System.out.println("This is my first step to ultralearning.~!");
+		System.out.println("My code has been changed.");
 		for(int i = 1; i <= 10; ++i) {
 			System.out.println("line : " + i);
 		}
